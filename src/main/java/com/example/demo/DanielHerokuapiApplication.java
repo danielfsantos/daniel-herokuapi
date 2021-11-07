151f483b4a0603eb2d34463860ba54df4bf9ec53
Daniel Fernandes <danielfsantos56@gmail.com>
2021-11-06 22:29:58 -0300

    make it better

--- src/main/java/com/example/demo/DanielHerokuapiApplication.java
+++ src/main/java/com/example/demo/DanielHerokuapiApplication.java
@@ -2,10 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class DanielHerokuapiApplication {
 
+	
+	   @RequestMapping("/")
+	    @ResponseBody
+	    String home() {
+	      return "Hello World!";
+	    }
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DanielHerokuapiApplication.class, args);
 	}
